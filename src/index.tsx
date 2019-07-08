70cac91c88a09d13242e53d2da3d6d11cbbed102
danort <daniel@torre.co>
2019-07-08 10:40:17 -0500

    added styletron-react to handle styles and basic Home component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { Client as Styletron } from 'styletron-engine-atomic';
 import * as serviceWorker from './serviceWorker';
 
+import Home from './pages/Home';
+
+// styles
 import './css/reset.css';
 import './css/fonts.css';
 
+const engine = new Styletron();
+
 ReactDOM.render(
-  <h1> The answer is 42 </h1>,
+  <StyletronProvider value={engine}>
+    <Home />
+  </StyletronProvider>,
   document.getElementById('root')
 );
 
