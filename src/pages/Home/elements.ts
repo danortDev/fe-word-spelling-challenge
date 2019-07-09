9cda5f3da5249365e6b1ce9652f9a0bfde9a5cd7
danort <daniel@torre.co>
2019-07-08 20:53:06 -0500

    refactor home layout (still figuring out how to place components)

--- src/pages/Home/elements.ts
+++ src/pages/Home/elements.ts
@@ -6,19 +6,27 @@ export const MainContainer = styled('main', {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
+  alignItems: 'center',
   background: `${getColor('LIGHTGREY')}`,
   minHeight: `calc(100vh - ${APP_BAR_HEIGHT})`
 });
 
-export const Card = styled('div', {
+export const ChallengeWrapper = styled('div', {
   width: '60%',
-  height: '160px',
+  boxShadow: `2px 2px 6px 0px ${getColor('BLACK').setAlpha(0.54)}`
+});
+
+export const Card = styled('section', {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
   padding: '40px',
   borderRadius: '4px',
-  margin: '0 auto',
   background: `${getColor('WHITE')}`,
-  boxShadow: `2px 2px 6px 0px ${getColor('BLACK').setAlpha(0.54)}`
+});
+
+export const Actions = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
 });
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import AppBar from 'app/components/ui/AppBar';
+import Button from 'app/components/ui/Button';
 import Word from 'app/components/ui/Word';
-import { MainContainer, Card } from './elements';
+import WordPronunciationButton from 'app/components/challenge/WordPronunciationButton';
+import { MainContainer, ChallengeWrapper, Card, Actions } from './elements';
 
 const Home = () => (
   <div>
     <AppBar />
     <MainContainer>
-      <Card>
-        <Word />
-      </Card>
+      <ChallengeWrapper>
+        <Card>
+          <Word />
+          <WordPronunciationButton
+            word="bazinga"
+          />
+        </Card>
+        <Actions>
+          <Button
+            fullWidth
+            text="Review"
+            onClick={() => {}}
+          />
+        </Actions>
+      </ChallengeWrapper>
     </MainContainer>
   </div>
 );
