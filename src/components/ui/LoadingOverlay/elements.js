affa384c0f6d25dd76ccb047182509733e26a355
danort <daniel@torre.co>
2019-07-10 10:25:02 -0500

    add Loading components

---
+++ src/components/ui/LoadingOverlay/elements.js
@@ -0,0 +1,24 @@
+import { styled } from 'styletron-react';
+import getColor from 'app/palette';
+
+export const Overlay = styled('div', {
+  minHeight: '100vh',
+  position: 'absolute',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  background: `${getColor('BLACK').setAlpha(.8)}`,
+  zIndex: 10
+});
+
+export const SpinnerWrapepr = styled('div', {
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
