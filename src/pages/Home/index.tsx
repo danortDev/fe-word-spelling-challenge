21c050464177b2d50abe70c65737360591bc50ec
danort <daniel@torre.co>
2019-07-10 10:26:55 -0500

    fetch exercises from home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useChallengeContext } from 'app/contexts/challenge';
-import AppBar from 'app/components/ui/AppBar';
+import { useGetChallenge } from 'app/contexts/challenge/actions';
+import App from 'app/components/layouts/App';
+import LoadingOverlay from 'app/components/ui/LoadingOverlay';
 import ProgressBar from 'app/components/ui/ProgressBar';
 import Exercise from 'app/components/challenge/Exercise';
 import LastStep from 'app/components/challenge/LastStep';
-import { MainContainer } from './elements';
+import ErrorLayout from 'app/components/challenge/ErrorLayout';
 
 const Home = () => {
   const [challenge] = useChallengeContext();
+  const [mounted, setMounted] = useState(false);
+  const getChallenge = useGetChallenge();
+
+  useEffect(() => {
+    console.log('I am here');
+    if (!mounted) {
+      console.log('I am making the request');
+      setMounted(true);
+      getChallenge();
+    }
+  }, [getChallenge, mounted]);
+
+  if (challenge.error) {
+    return (
+      <App>
+        <ErrorLayout />
+      </App>
+    );
+  }
+
+  if (challenge.loading) {
+    return (
+      <App>
+        <LoadingOverlay />
+      </App>
+    );
+  }
+
   const { currentExercise, exercises, currentIndex, solved } = challenge;
   const progress = currentIndex * 100 / exercises.length;
 
   return (
-    <div>
-      <AppBar />
-      <MainContainer>
-        {!solved && (
-          <Exercise exercise={currentExercise} />
-        )}
-        {solved && (
-          <LastStep />
-        )}
-        <ProgressBar progress={progress} />
-      </MainContainer>
-    </div>
+    <App>
+      {!solved && (
+        <Exercise exercise={currentExercise} />
+      )}
+      {solved && (
+        <LastStep />
+      )}
+      <ProgressBar progress={progress} />
+    </App>
   );
 };
 
